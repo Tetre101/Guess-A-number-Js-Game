c3b9dfe8878d7e9ed04853153ee8898baf55e06f
Tetre101 <108693816+Tetre101@users.noreply.github.com>
2023-01-16 20:53:24 +0100

    Uploaded my First Javascript Game Files
    I enjoyed every part of creating this game from start to finish and hope you enjoy playing this game

---
+++ script.js
@@ -0,0 +1,87 @@
+'use strict';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Pick the Number 😒';
+// document.querySelector('.score').textContent = 20;
+// document.querySelector('.highscore').textContent = 50;
+// document.querySelector('.guess').value = 40;
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  //   when there's no input
+  if (!guess) {
+    // document.querySelector('.message').textContent = 'no number🤦‍♂️';
+    displayMessage('no number🤦‍♂️');
+
+    // when player wins
+  } else if (guess === secretNumber) {
+    // document.querySelector('.message').textContent = 'Correct number 👍';
+    displayMessage('Correct number 👍');
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = 'Green';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+
+    // when a higher number is picked
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      // document.querySelector('.message').textContent =
+      //   guess > secretNumber
+      //     ? 'You picked a higer number, try again😒'
+      //     : 'You guessed below bar,take it higher🤦‍♂️🤷‍♀️';
+      displayMessage(
+        guess > secretNumber
+          ? 'You picked a higer number,try again😒'
+          : 'You guessed below bar,take it higher🤦‍♂️🤷‍♀️'
+      );
+      score = score - 1;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('body').style.backgroundColor = 'purple';
+      document.querySelector('.number').style.width = '10rem';
+    } else {
+      // document.querySelector('.message').textContent = 'you lost the game😥';
+      displayMessage('you lost the game😥');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+  // } else if (guess > secretNumber) {
+  //   // when a lower number is picked
+  // } else if (guess < secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent =
+  //       'You guessed below bar,take it higher🤦‍♂️🤷‍♀️';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //     document.querySelector('body').style.backgroundColor = 'brown';
+  //     document.querySelector('.number').style.width = '10rem';
+  //   } else {
+  //     document.querySelector('.message').textContent = 'you lost the game😥';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  // }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').textContent = '?';
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  // document.querySelector('.message').textContent = 'start guessing...';
+  displayMessage('start guessing...');
+  document.querySelector('body').style.backgroundColor = 'pink';
+  document.querySelector('number').style.width = '15rem';
+});
